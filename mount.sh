79d24ca600bad525190ad3198ecdfaf0e7cb282d
朱伟亮 <zhuwl120820@gxwsxx.com>
2016-07-21 15:38:37 +0800

    添加测试环境的挂载

--- mount.sh
+++ mount.sh
@@ -12,12 +12,12 @@ mount -t nfs 192.168.200.29:/home/tomcat-cluster/tomcat-upload/lezcashier /home/
 
 
 #测试
-#mount -t nfs 192.168.200.18:/home/tomcat-cluster/tomcat-upload /home/www/test/up
-#mount -t nfs 192.168.200.22:/home/tomcat-cluster/tomcat-upload/weixin /home/www/test/up/weixin
-#mount -t nfs 192.168.200.22:/home/tomcat-cluster/tomcat-upload/alipay /home/www/test/up/alipay
-#mount -t nfs 192.168.200.17:/home/tomcat-cluster/tomcat-upload/web-weather-media /home/www/test/up/web-weather-media
-#mount -t nfs 192.168.200.17:/home/tomcat-cluster/tomcat-upload/web-qimeng /home/www/test/up/web-qimeng
-#mount -t nfs 192.168.200.17:/home/tomcat-cluster/tomcat-upload/web-lvye /home/www/test/up/web-lvye
-#mount -t nfs 192.168.200.17:/home/tomcat-cluster/tomcat-upload/web-tdsp /home/www/test/up/web-tdsp
-#mount -t nfs 192.168.200.17:/home/tomcat-cluster/tomcat-upload/manage-lezcashier /home/www/test/up/manage-lezcashier
-#mount -t nfs 192.168.200.17:/home/tomcat-cluster/tomcat-upload/web-weather-new-media /home/www/test/up/web-weather-new-media
+mount -t nfs 192.168.200.18:/home/tomcat-cluster/tomcat-upload /home/www/test/test/up
+mount -t nfs 192.168.200.22:/home/tomcat-cluster/tomcat-upload/weixin /home/www/test/test/up/weixin
+mount -t nfs 192.168.200.22:/home/tomcat-cluster/tomcat-upload/alipay /home/www/test/test/up/alipay
+mount -t nfs 192.168.200.17:/home/tomcat-cluster/tomcat-upload/web-weather-media /home/www/test/test/up/web-weather-media
+mount -t nfs 192.168.200.17:/home/tomcat-cluster/tomcat-upload/web-qimeng /home/www/test/test/up/web-qimeng
+mount -t nfs 192.168.200.17:/home/tomcat-cluster/tomcat-upload/web-lvye /home/www/test/test/up/web-lvye
+mount -t nfs 192.168.200.17:/home/tomcat-cluster/tomcat-upload/web-tdsp /home/www/test/test/up/web-tdsp
+mount -t nfs 192.168.200.17:/home/tomcat-cluster/tomcat-upload/manage-lezcashier /home/www/test/test/up/manage-lezcashier
+mount -t nfs 192.168.200.17:/home/tomcat-cluster/tomcat-upload/web-weather-new-media /home/www/test/test/up/web-weather-new-media
